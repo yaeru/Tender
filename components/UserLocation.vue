889bb3f08f08722f3f2d6201d411a37a4f192824
Yael Duckwen <yaelduckwen@gmail.com>
2024-03-02 18:23:20 -0300

    no composable

--- components/UserLocation.vue
+++ components/UserLocation.vue
@@ -1,16 +1,4 @@
-<script setup>
-	//import { useGeolocation } from "@/composables/useGeoLocation";
-
-	const {kordinat} = useGeolocation()
-	const posisiUser = computed(()=>({
-		lat : kordinat.value.latitude,
-		lng : kordinat.value.longitude
-	}))
-	watchEffect(() => {
-		console.log({ p: posisiUser.value });
-	})
-</script>
 
 <template>
-	<h2>Coords : {{posisiUser.lat}}, {{posisiUser.lng}}</h2>
+	pepe
 </template>
